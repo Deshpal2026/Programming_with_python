3d4e9b8b1c00387eded3d43ccec7dde78bf69733
Desh Deepak Pal <dp210346@gmail.com>
2026-02-03 19:34:26 +0530

    added

---
+++ rangefunction.py
@@ -0,0 +1,13 @@
+
+n = int(input("Enter a natural number: "))
+sum = 0
+for i in range( 1, n+1 ):
+    sum += i
+print(sum)
+#This code calculates the sum of first n natural numbers using range function
+num = 0
+for i in range(1, 6):
+    num += i
+print(num)    
+
+    
