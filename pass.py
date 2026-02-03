aecaafea49e10f4c12a2f81b2ed436db4ba12af2
Desh Deepak Pal <dp210346@gmail.com>
2026-02-03 15:57:13 +0530

    learning

---
+++ pass.py
@@ -0,0 +1,9 @@
+for i in range(0, 100,2):
+    pass
+
+#pass function is used to create an empty block of code 
+#it is a placeholder that does nothing when executed 
+#it is useful when a statement is syntactically required but no action is needed
+#for example in loops, functions, classes or conditional statements
+
+print("some useful tasks using range function are done successfully ")    
---
+++ usingrangefun.py
@@ -0,0 +1,9 @@
+n = int(input("Enter your number: ")) #Taking input from the user 
+for i in range(1, 11, 1):#Using range functin to generate umbers from 1 to 10
+  print(i*n)
+print("\n")
+
+for i in range(10, 0, -1):
+  print(i*n, end=" ") 
+   
+print(" \n code is ended")
