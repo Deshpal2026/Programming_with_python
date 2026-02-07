bc95669e71da644c912aba6c9401086d4317bf17
Desh Deepak Pal <dp210346@gmail.com>
2026-02-07 14:39:35 +0530

    added

---
+++ fun.py
@@ -0,0 +1,14 @@
+
+# def cal_average( a, b, c):
+#     sum = a + b + c
+#     average = sum / 3
+#     print("Average:", average)
+#     return average
+# cal_average(5, 9, 6)
+subject = ["hindi", "english", "Mathematics", "Science"]
+
+def sub_call_1 (list):
+    print(len(list))
+    
+sub_call_1(subject)    
+    
