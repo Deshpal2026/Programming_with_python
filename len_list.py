db19f2c9e90a6a1c4bc32f237506155a159fca5c
Desh Deepak Pal <dp210346@gmail.com>
2026-02-11 19:15:41 +0530

    added

---
+++ len_list.py
@@ -0,0 +1,7 @@
+cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"]
+heroes = ["Superman", "Batman", "Wonder Woman"]
+
+def count_items(items):
+    print(len(items))
+count_items(cities)
+count_items(heroes)
