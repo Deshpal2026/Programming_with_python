a79097de2c83a8f38f53d1b68cb5ad39e479ae76
Desh Deepak Pal <dp210346@gmail.com>
2026-03-18 15:27:17 +0530

    added

---
+++ pythonfile.py/RenntCalculater.py
@@ -0,0 +1,6 @@
+# using basic concepts of python to develop a Rental Calculater
+rent = int(input("Enter the rent of room / house ="))
+months = int(input("Enter the number of months ="))
+total_rent = rent * months
+print("Total rent for", months, "months is =", total_rent)
+
